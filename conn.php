8d73ded98a58a2466efcc2f2908ebd0ba59bb91d
laptop hp 242 g1 <ambicopayroll@gmail.com>
2017-04-06 14:06:04 +0700

    update conn
    update conn

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 //echo "1: ".$_SERVER["REMOTE_ADDR"];
 //echo "2: ".$_SERVER["HTTP_POST"];
-if ($_SERVER["REMOTE_ADDR"] == "127.0.0.1"  || $_SERVER["REMOTE_ADDR"] == "::1"  || $_SERVER["HTTP_POST"] == "localhost" ) {
+if ($_SERVER["HTTP_POST"] == "localhost" ) {
 	$hostname_conn = "localhost";
 	$database_conn = "fin_pro"; //$database_conn = "zecorind_mitra2";
 	$username_conn = "root"; //$username_conn = "zecorind_root";
