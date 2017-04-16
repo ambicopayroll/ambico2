cf715e02b4f48810fbd4517e4c4bfa45701a90cc
laptop hp 242 g1 <ambicopayroll@gmail.com>
2017-04-17 06:40:29 +0700

    1

--- payroll.sql
+++ payroll.sql
@@ -1,16 +1,12 @@
-select
-	a.pegawai_id
-    , a.pegawai_nama
-    , a.pegawai_nip
-    , b.*
-from
-	pegawai a
-    left join (select * from t_jdw_krj_def where cast(tgl as date) between cast('2017-03-02' as date) and cast('2017-03-08' as date)) b on a.pegawai_id = b.pegawai_id
--- where
-	
--- group by
-	-- a.pegawai_id
-    
+-- select * from v_rekon
+
+select * from
+	v_rekon a
+    left join t_rumus_peg b on a.pegawai_id = b.pegawai_id
+    left join t_rumus c on b.rumus_id = c.rumus_id
+where
+	tgl between '2017-03-02' and '2017-03-08'
+    and c.hk_gol = a.gol_hk
 order by
-	a.pegawai_nip
-    , b.tgl
+	a.pegawai_id
+    , tgl
