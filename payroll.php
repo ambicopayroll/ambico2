41fbf7ee9258b857e8e39348e1ba3ab7c79bce08
laptop hp 242 g1 <ambicopayroll@gmail.com>
2017-04-16 13:44:44 +0700

    1:44 PM 4/16/2017
    payroll

--- payroll.php
+++ payroll.php
@@ -18,7 +18,7 @@ $msql = "
 		left join t_rumus_peg b on a.pegawai_id = b.pegawai_id
 		left join t_rumus c on b.rumus_id = c.rumus_id
 	where
-		tgl between '2017-03-02' and '2017-03-08'
+		tgl between '".$_POST['start']."' and '".$_POST['end']."'
 		and c.hk_gol = a.gol_hk
 	order by
 		a.pegawai_id
